fae8cf748c09ab3ac20c5e1b4d7be51ba2220639
Edixon V <ingedixonvargas@gmail.com>
2017-01-12 16:38:44 -0300

    Updating documentation

--- INSYDE/doc/reference/html/navtreedata.js
+++ INSYDE/doc/reference/html/navtreedata.js
@@ -26,21 +26,12 @@ var NAVTREE =
 var NAVTREEINDEX =
 [
 ".html",
-<<<<<<< HEAD
 "class_effect.html#aa79a7efdd14bd17280a0a93658d44451",
 "class_weight_line.html#a8362e5e013cb69a4c7145b2911a45c39",
 "classcore_1_1_clipboard_interface.html#aa32a73262fd066143ed166a794d519e4",
 "classcore_1_1_image.html#a3c0ab59560f62ac139bfa208809e3a6b",
 "classcore_1_1_normalization_widget.html#a730d7d429f38f4552614822b34f3b557",
 "structann__base_1_1_adaline_1_1_train_result.html#a31ad1357a6e0361dbe4a377f9d2fa6da"
-=======
-"class_effect.html#a7eec3ba2ce8813b821e41c3454e1607b",
-"class_weight_line.html#a82258680f98ea3e92f468364e1128950",
-"classcore_1_1_clipboard_interface.html#aa2d13e4223c20a8c2a27bb9a289707d4",
-"classcore_1_1_image.html#a3b17f4943426ce799284bbb8f56ec0f9",
-"classcore_1_1_normalization_widget.html#a371ea2ad3293d144f70f62218a1bd1b9",
-"structann__base_1_1_adaline_1_1_train_result.html#a379c5b1188105f9b8fd6384d78ad5859"
->>>>>>> 5a80891754bc958e07c66864a6337f3bd51bd9c3
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
